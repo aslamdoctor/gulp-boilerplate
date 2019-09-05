2888994975463590b76845614c16b6bcb1217932
Aslam Doctor <aslam.doctor@gmail.com>
2019-09-05 08:20:18 +0530

    added wp theme support

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const sass = require("gulp-sass"); // to add SASS support
 const uglify = require('gulp-uglify'); // to minimize JS files code
 const concat = require('gulp-concat'); // to concat JS files
 
+var theme_folder="./wp-content/themes/boilerplate-theme/"
+
 // add all the css/sass files here to compile
 var css_files = [
 	'./src/js/**/*.css', 
@@ -23,9 +25,10 @@ var js_files = [
 	'./src/js/scripts.js',
 ]
 
-// add all icon files here to compile
-var icon_files = [
+// add all font files here to compile
+var font_files = [
 	'./src/css/fontawesome/webfonts/*',
+	'./src/fonts/*',
 ]
 
 
@@ -33,6 +36,10 @@ var icon_files = [
 var cleanup_files = [
 	'./dist/css/',
 	'./dist/js/',
+]
+var cleanup_files_wp = [
+	theme_folder + 'dist/css/',
+	theme_folder + 'dist/js/',
 ]
 
 
@@ -51,6 +58,20 @@ function css() {
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('dist/assets/css/'));
 }
+function css_wp() {
+	return gulp.src(css_files)
+		.pipe(sourcemaps.init())
+		.pipe(sass({ outputStyle: "expanded" }))
+		.pipe(concat('styles.min.css'))
+		.pipe(postcss([
+			autoprefixer({
+				cascade: false
+			}), 
+			cssnano()
+		]))	
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(theme_folder + 'dist/assets/css/'));
+}
 
 
 // JS Task
@@ -60,18 +81,31 @@ function js() {
 		.pipe(concat('scripts.min.js'))
 		.pipe(gulp.dest('dist/assets/js/', { sourcemaps: false }));
 }
+function js_wp() {
+	return gulp.src(js_files, { sourcemaps: true })
+		.pipe(uglify())
+		.pipe(concat('scripts.min.js'))
+		.pipe(gulp.dest(theme_folder + 'dist/assets/js/', { sourcemaps: false }));
+}
 
 
-// Icons Task
-function icons() {
-	return gulp.src(icon_files)
+// Fonts Task
+function fonts() {
+	return gulp.src(font_files)
 		.pipe(gulp.dest('dist/assets/webfonts/'));
+}
+function fonts_wp() {
+	return gulp.src(font_files)
+		.pipe(gulp.dest(theme_folder + 'dist/assets/webfonts/'));
 }
 
 
 // Cleanup Task
 function clean() {
 	return del(cleanup_files);
+}
+function clean_wp() {
+	return del(cleanup_files_wp);
 }
 
 
@@ -83,23 +117,41 @@ function watchFiles() {
 	// Watch js files changes
 	gulp.watch(js_files, js);
 	
-	// Watch icons files changes
-	gulp.watch(icon_files, icons);
+	// Watch fonts files changes
+	gulp.watch(font_files, fonts);
+}
+function watchFiles_wp() {
+	// Watch sass/css files changes
+	gulp.watch(css_files, css_wp);
+	
+	// Watch js files changes
+	gulp.watch(js_files, js_wp);
+	
+	// Watch fonts files changes
+	gulp.watch(font_files, fonts_wp);
 }
 
 // Watch Task
 const watch = gulp.parallel(watchFiles);
+const watchwp = gulp.parallel(watchFiles_wp);
 
 // Build Task
-const build = gulp.series(clean, gulp.parallel(icons, css, js));
+const build = gulp.series(clean, gulp.parallel(fonts, css, js));
+const buildwp = gulp.series(clean_wp, gulp.parallel(fonts_wp, css_wp, js_wp));
 
 
 // Export all tasks
 exports.css = css;
+exports.csswp = css_wp;
 exports.js = js;
-exports.icons = icons;
+exports.jswp = js_wp;
+exports.fonts = fonts;
+exports.fontswp = fonts_wp;
 
 exports.clean = clean;
+exports.cleanwp = clean_wp;
 exports.watch = watch;
+exports.watchwp = watchwp;
 exports.build = build;
+exports.buildwp = buildwp;
 exports.default = build;
